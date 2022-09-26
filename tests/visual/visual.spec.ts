8c1ee7ffca8d3f235a49bc0338d68d638468fc10
allaaMuhammed <allaamuhammed.7@gmail.com>
2022-09-26 20:39:31 +0200

    Visual test for single element
    Adding visual test for single element in the web page

--- tests/visual/visual.spec.ts
+++ tests/visual/visual.spec.ts
@@ -5,4 +5,11 @@ test.describe("Visual Regression Testing Example", () =>{
         await page.goto("https://www.example.com")
         expect(await page.screenshot()).toMatchSnapshot('homepage.png')
     })
+
+    test.only("Single Element Snapshot",async ({page}) => {
+        await page.goto("https://www.example.com")
+
+        const pageElement = await page.$("h1")
+        expect(await pageElement?.screenshot()).toMatchSnapshot('page-title.png')
+    })
 })
