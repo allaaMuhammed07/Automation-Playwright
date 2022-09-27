06dd75ce4eeb726025fb8f4821713c85b40b6b3c
allaaMuhammed <allaamuhammed.7@gmail.com>
2022-09-27 18:56:48 +0200

    Update api-delete-tests.spec.ts

--- tests/api/api-delete-tests.spec.ts
+++ tests/api/api-delete-tests.spec.ts
@@ -2,7 +2,7 @@ import {test, expect} from "@playwright/test"
 
 const baseURL: string = "https://reqres.in/api/"
 
-test.only("DELETE Request - Update user",async ({request}) => {
+test("DELETE Request - Update user",async ({request}) => {
     const response = await request.delete(`${baseURL}users/2`)
     expect(response.status()).toBe(204)
 })
