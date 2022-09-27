d2b2942486e58e64a7b4f2d2cddd963217901719
allaaMuhammed <allaamuhammed.7@gmail.com>
2022-09-27 17:32:06 +0200

    POST Request updates
    branch update

--- tests/api/api-post-tests.spec.ts
+++ tests/api/api-post-tests.spec.ts
@@ -1,48 +1,50 @@
 import {test, expect} from "@playwright/test"
 
 const baseURL: string = "https://reqres.in/api/"
-    let firstName = "morpheus"
-    let job = "leader"
+let firstName = "morpheus"
+let job = "leader"
+let validUserEmail = "eve.holt@reqres.in"
+let validUserPassword = "cityslicka"
+let invalidUserEmail = "peter@klaven"
+let errorMessage = "Missing password"
 
-test("POST Request - Create new user",async ({request}) => {
-
-
-    const response = await request.post(`${baseURL}users`,{
-        data:{
-            "name": firstName,
-            "job": job,
-        }
+test.describe.only("POST Request Test", ()=>{
+    test("POST Request - Create new user",async ({request}) => {
+        const response = await request.post(`${baseURL}users`,{
+            data:{
+                "name": firstName,
+                "job": job,
+            }
+        })
+    
+        const responseBody = JSON.parse(await response.text())
+        expect(response.status()).toBe(201)
+        expect(responseBody.name).toBe(firstName)
+        expect(responseBody.createdAt).toBeTruthy()
     })
-
-    const responseBody = JSON.parse(await response.text())
-    expect(response.status()).toBe(201)
-    expect(responseBody.name).toBe(firstName)
-    expect(responseBody.createdAt).toBeTruthy()
-})
-
-test("POST Request - Login",async ({request}) => {
-    const response = await request.post(`${baseURL}login`,{
-        data:{
-            email:"eve.holt@reqres.in",
-            password:"cityslicka"
-        }
+    
+    test("POST Request - Login",async ({request}) => {
+        const response = await request.post(`${baseURL}login`,{
+            data:{
+                email: validUserEmail,
+                password: validUserPassword
+            }
+        })
+    
+        const responseBody = JSON.parse(await response.text())
+        expect(response.status()).toBe(200)
+        expect(responseBody.token).toBeTruthy()
     })
-
-    const responseBody = JSON.parse(await response.text())
-    expect(response.status()).toBe(200)
-    expect(responseBody.token).toBeTruthy()
-
-    console.log(responseBody)
-})
-
-test.only("POST Login - Login Failed",async ({request}) => {
-    const response = await request.post(`${baseURL}login`,{
-        data:{
-            email:"peter@klaven"
-        }
+    
+    test("POST Login - Login Failed",async ({request}) => {
+        const response = await request.post(`${baseURL}login`,{
+            data:{
+                email: invalidUserEmail
+            }
+        })
+    
+        const responseBody = JSON.parse(await response.text())
+        expect(response.status()).toBe(400)
+        expect(responseBody.error).toBe(errorMessage)
     })
-
-    const responseBody = JSON.parse(await response.text())
-    expect(response.status()).toBe(400)
-    expect(responseBody.error).toBe("Missing password")
 })
